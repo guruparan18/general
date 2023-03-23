32d4b7ec3d2b1b50b5dfeffc7261dbe092382602
Sree Appachi (PK5T) <guruparan18@gmail.com>
2023-03-22 21:48:41 -0400

    reorg -dirs

---
+++ python/prime/src/primes.py
@@ -0,0 +1,23 @@
+import sys
+
+primes = [2]
+
+
+def is_prime(num: int) -> bool:
+    for prime in primes:
+        if prime > num ** (1 / 2):
+            return True
+        if num % prime == 0:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    number = 50000
+    if len(sys.argv) == 2:
+        number = int(sys.argv[1])
+    for num in range(3, number, 2):
+        if is_prime(num):
+            primes.append(num)
+
+    print(len(primes))
